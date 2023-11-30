a0af8a82e33768c121f4e278936f966047972f77
CodeByGzt <108520072+CodeByGzt@users.noreply.github.com>
2023-11-30 08:33:40 +0000

    🐞 fix: 修改部分设置以及头像 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/21/Docker学习笔记/","2023/08/01/Typora + PicGo + OSS 配置图床/","2023/11/29/hello-world/"];function toRandomPost(){
+var posts=["2023/10/21/Docker学习笔记/","2023/08/01/Typora + PicGo + OSS 配置图床/","2023/11/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
